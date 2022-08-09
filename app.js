4693280880f9d55d6b9b0484621d99ab50766c8c
maxrb0 <mramirezborches@gmail.com>
2022-08-09 19:56:42 -0300

    todo ordenado y links a login y register

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3030, ()=>{
+    console.log('Servidor funcionando');
+});
