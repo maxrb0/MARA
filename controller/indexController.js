5da897562360b2bc1793643513e89bc67be4b6a5
santileon046@gmail.com <santileon046@gmail.com>
2022-09-01 21:06:27 -0500

    Home ya es dinamico

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,9 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+
+const productsFilePath = path.join(__dirname, "../Data/productsData.json");
+
 const indexController = {
     home:(req,res)=>{
+        const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
+        const laLiga = products.filter((p) => p.category == "La liga");
+        const PremierLeague = products.filter((p) => p.category == "Premier League");
+        const SeleccionesDelMundo = products.filter((p) => p.category == "Selecciones Del Mundo");
+        const PrimeraDivisionColombiana = products.filter((p) => p.category == "Primera Division Colombiana");
+        const PrimeraDivisionArgentina = products.filter((p) => p.category == "Primera Division Argentina");
 
-        
-        res.render("home")
+        res.render("home", {
+            productos: products,
+            Laliga: laLiga,
+            PremierLeague: PremierLeague,
+            SeleccionesDelMundo: SeleccionesDelMundo,
+            PrimeraDivisionColombiana: PrimeraDivisionColombiana,
+            PrimeraDivisionArgentina: PrimeraDivisionArgentina
+          });
     },
     cart:(req,res)=>{
         res.render("productCart")
